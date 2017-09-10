9bd7d7ba7e2f8840db143ad8ac134271a71b44c6
Mike Dacanay <migd64@gmail.com>
2017-09-10 04:06:29 -0700

    / route works as intended

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express = require("express");
 var app = express();
 var session = require('express-session');
 var bodyParser = require('body-parser');
-app.use(session({secret: 'codingdojorocks'})); 
 
+app.use(session({secret: 'codingdojorocks'})); 
 app.use(bodyParser.urlencoded({extended: true}));
-app.listen(8000, function() {
-})
+app.listen(8000, function() {})
 app.set('views', __dirname + '/views'); 
 app.set('view engine', 'ejs');
+
+app.get("/", function (request, response){
+	response.send("<h1>Hello Express</h1>");
+})

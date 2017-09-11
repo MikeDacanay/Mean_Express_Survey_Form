0a5b9ff882e33657fe33dd163ae9621361ed2f0d
Mike Dacanay <migd64@gmail.com>
2017-09-10 18:25:39 -0700

    all routes linked, work on final get template

--- server.js
+++ server.js
@@ -12,3 +12,21 @@ app.set('view engine', 'ejs');
 app.get("/", function (request, response){
     response.render('index');
 })
+
+app.post("/add", function (request, response){
+	request.session.name=request.body.name;
+	request.session.location=request.body.location;
+	request.session.language=request.body.language;
+	request.session.comment=request.body.comment;
+    response.redirect('/results')
+})
+
+app.get("/results", function (request, response){
+	info= {
+		name: request.session.name,
+		location: request.session.location,
+		language: request.session.language,
+		comment: request.session.comment
+	}
+    response.render('results',{info: info});
+})
